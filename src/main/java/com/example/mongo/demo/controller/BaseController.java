e84157e762ff9418080bd252380d7ec2d308d304
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2022-01-03 10:51:38 -0300

    adicionar serviço

--- src/main/java/com/example/mongo/demo/controller/BaseController.java
+++ src/main/java/com/example/mongo/demo/controller/BaseController.java
@@ -1,8 +1,10 @@
 package com.example.mongo.demo.controller;
 
 import com.example.mongo.demo.docs.Customer;
+import com.example.mongo.demo.docs.SiafNC;
 import com.example.mongo.demo.model.APIResponse;
 import com.example.mongo.demo.repositories.CustomerRepository;
+import com.example.mongo.demo.repositories.SiafNCRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,9 @@ public class BaseController {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private SiafNCRepository siafNCRepository;
 	
 	@GetMapping(value = "/base")
     public ResponseEntity<APIResponse> readBaseResponse () {
@@ -34,6 +39,11 @@ public class BaseController {
     @GetMapping(value = "/customers")
     public ResponseEntity<List<Customer>> customers () {
         return ResponseEntity.ok(customerRepository.findAll());
+    }
+
+    @GetMapping(value = "/siafs")
+    public ResponseEntity<List<SiafNC>> siafs () {
+        return ResponseEntity.ok(siafNCRepository.findAll());
     }
 
 }
